68f3c869b7b29a6e126e26c42da90f8b1452fd78
Rishikesh1235 <rishikesh.bhagat24@vit.edu>
2024-12-13 10:04:04 +0530

    index
    commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.querySelector('.login-btn').addEventListener('click', function() {
+    alert('Login functionality is not implemented in this demo.');
+});
+
+document.querySelector('.start-btn').addEventListener('click', function() {
+    alert('Get Started functionality is not implemented in this demo.');
+});
